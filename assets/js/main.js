54c154cca1af0dab4d608b2ff413ebb3a2285e50
Hafsa <hafsajabeen47@gmail.com>
2024-11-16 00:26:52 +0500

    New Index Page for Personal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,17 @@ let toggleBtn = document.querySelector('#toggle-btn');
 let toggleX = document.querySelector('#toggle-x');
 
 
-let nav = document.querySelector('.nav');
+
+let nav = document.querySelector('nav');
+if(window.innerWidth < 939){
     nav.style.display = 'none';
-toggleBtn.addEventListener('click', ()=>{
-    nav.style.display = 'flex';
-    toggleBtn.style.display = 'none';
-    toggleX.style.display = 'block';
-});
+}
+
+
+// let nav = document.querySelector('.nav');
+//     nav.style.display = 'none';
+// toggleBtn.addEventListener('click', ()=>{
+//     nav.style.display = 'flex';
+//     toggleBtn.style.display = 'none';
+//     toggleX.style.display = 'block';
+// });
